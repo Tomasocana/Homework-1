118a4d5b64a397eb9412a6f3309cf4f83ba63768
tomasocana <tocana@udesa.edu.com>
2025-03-12 12:01:27 -0300

    punto 1 terminado, arrancando punto 2

--- Ejercicio1/eje.cpp
+++ Ejercicio1/eje.cpp
@@ -2,18 +2,20 @@
 using namespace std;
 
 // (A)
-int square_matrix(int n){
-    if (n < 1) cout << "Error: n debe ser positivo y mayor a 1" << "\n"; return 0;
+int** square_matrix(int n){
+    if (n < 1){
+        cout << "Error: n debe ser positivo y mayor a 1" << "\n"; 
+        return nullptr;
+    }
 
-    int matrix[n];
+    int** matrix = new int*[n];
     int flag = 1;
-    for (const int i: matrix){
-        int line[n];
-        for (const int j = 0; j < n; j++){
-            line[j] = flag;
+    for (int i = 0; i < n; i++){
+        matrix[i] = new int[n];
+        for (int j = 0; j < n; j++){
+            matrix[i][j] = flag;
             flag++;
         }
-        matrix[i] = line;
     }
     return matrix;
     
@@ -24,10 +26,12 @@ int main(){
     int n;
     cout << "Ingrese el tamaño de la matriz cuadrada: ";
     cin >> n;
-    matrix = square_matrix(n);
+    int** matrix = square_matrix(n);
+    if (matrix == nullptr) return 1;
     int fila = n - 1;
     int columna = n - 1;
-    for (int i = 0; i < n**2; i++){
+    int end = n*n;
+    for (int i = 0; i < end; i++){
         cout << "M" << n << "[" << fila << "][" << columna << "] = " << matrix[fila][columna] << "\n";
         if (columna > 0){
             columna--;
---
+++ Ejercicio2/eje.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+enum class errores {DEBUG, INFO, WARNING, ERROR, CRITICAL};
+
+// (a)
+
+string evento(errores NivelSeveridad){
+    switch (NivelSeveridad){
+        case errores::DEBUG:
+            return "DEBUG";
+        case errores::INFO:
+            return "INFO";
+        case errores::WARNING:
+            return "WARNING";
+        case errores::ERROR:
+            return "ERROR";
+        case errores::CRITICAL:
+            return "CRITICAL";
+        default: 
+            cout << "No es un error contemplado";
+            return "";
+    }
+}
+
+void logMessage(string mensaje, errores NivelSeveridad) {
+    string gravedad = evento(NivelSeveridad);
+    ofstream file ("log.txt");
+    if (file.is_open()){
+        file << gravedad << ": " << mensaje;
+        file.close();
+    }
+    else cout << "No se pudo abrir el archivo";
+}
+
+int main(){
+    logMessage("Error en el sistema", errores::DEBUG);
+    return 0;
+}
+
+// (b)
+void logMessage(string Mensaje_de_Error, string archivo, int Línea_de_Código){
+    ofstream file ("log.txt");
+    if (file.is_open()){
+        file << "Error en el archivo: " << archivo << ", en la línea " << Línea_de_Código << ": " << Mensaje_de_Error;
+        file.close();
+    }
+    else cout << "No se pudo abrir el archivo";
+}
+
+void logMessage(string Mensaje_De_Acceso, string Nombre_de_Usuario){
+    ofstream file ("log.txt");
+    if (file.is_open()){
+        file << "[SCURITY] " << Nombre_de_Usuario << ": " << Mensaje_De_Acceso << ".\n Proceed.";
+        file.close();
+    }
+    else cout << "No se pudo abrir el archivo";
+}
+
+int main(){
+    
+}
